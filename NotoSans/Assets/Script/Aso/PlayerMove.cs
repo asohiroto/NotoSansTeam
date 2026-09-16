2d23e5c72cdd4ab72ef7bfd7ba0cb68c605f401c
asohiroto <fko2647059@stu.o-hara.ac.jp>
2026-09-16 18:38:29 +0900

    playerMove

--- NotoSans/Assets/Script/Aso/PlayerMove.cs
+++ NotoSans/Assets/Script/Aso/PlayerMove.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    // プレイヤーの移動速度
+    public static readonly Vector3 k_MoveSpeed = new Vector3(0.0f, 0.01f, 0.0f);
+
     [SerializeField] GameObject _playerObj;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _playerObj.transform.position += k_MoveSpeed;
     }
 }
