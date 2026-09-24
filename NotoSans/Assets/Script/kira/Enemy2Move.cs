b007173d66d5451a9d9c557821445de4924130bb
KIRAGRA <fko2647040@stu.o-hara.ac.jp>
2026-09-24 18:35:03 +0900

    20260924kira

--- NotoSans/Assets/Script/kira/Enemy2Move.cs
+++ NotoSans/Assets/Script/kira/Enemy2Move.cs
@@ -1,7 +1,10 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Enemy2Move: MonoBehaviour
 {
+    public float EnemyMoveSpeed = 100f;
+    public float EnemyDistance = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,16 @@ public class Enemy2Move: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + EnemyMoveSpeed * Time.deltaTime * Vector3.right;
+        //bool Raycastcolision = Physics.Raycast(1, 1, 1);
+        float checkX = transform.position.x + Mathf.Sign(EnemyMoveSpeed) * EnemyDistance;
+
+
+
+        if (transform.position.x >= 3 || transform.position.x <= -3)
+        {
+            EnemyMoveSpeed = -EnemyMoveSpeed;
+        }
+       
     }
 }
